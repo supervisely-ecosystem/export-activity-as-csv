569ed0b2b877ca5792caf7674a6fee4ec12ad144
cxnt <pavelbartsits@gmail.com>
2021-06-10 16:03:33 +0300

    progress bar

--- src/main.py
+++ src/main.py
@@ -2,7 +2,6 @@ import os
 import supervisely_lib as sly
 import pandas as pd
 
-
 my_app = sly.AppService()
 
 TEAM_ID = int(os.environ['context.teamId'])
@@ -18,34 +17,41 @@ logger = sly.logger
 @my_app.callback("download_activity_csv")
 @sly.timeit
 def download_activity_csv(api: sly.Api, task_id, context, state, app_logger):
-
     if PROJECT_ID:
         result_act = api.project.get_activity(int(PROJECT_ID))
-        file_remote = "/activity_data/{}_{}_{}".format(TASK_ID, PROJECT_ID, RESULT_FILE_NAME)
+        file_remote = f"/activity_data/{TASK_ID}_{PROJECT_ID}_{RESULT_FILE_NAME}"
     elif LABEL_JOB_ID:
         result_act = api.labeling_job.get_activity(int(LABEL_JOB_ID))
-        file_remote = "/activity_data/{}_{}_{}".format(TASK_ID, LABEL_JOB_ID, RESULT_FILE_NAME)
+        file_remote = f"/activity_data/{TASK_ID}_{LABEL_JOB_ID}_{RESULT_FILE_NAME}"
     elif MEMBER_ID:
         result_act = api.user.get_member_activity(TEAM_ID, int(MEMBER_ID))
-        file_remote = "/activity_data/{}_{}_{}".format(TASK_ID, MEMBER_ID, RESULT_FILE_NAME)
+        file_remote = f"/activity_data/{TASK_ID}_{MEMBER_ID}_{RESULT_FILE_NAME}"
     elif TEAM_ID:
         result_act_list = api.team.get_activity(TEAM_ID)
         columns = result_act_list[0].keys()
         result_act = pd.DataFrame(result_act_list, columns=columns)
+        file_remote = f"/activity_data/{TASK_ID}_{TEAM_ID}_{RESULT_FILE_NAME}"
 
-        file_remote = "/activity_data/{}_{}_{}".format(TASK_ID, TEAM_ID, RESULT_FILE_NAME)
-
-    app_logger.info("Remote file path: {!r}".format(file_remote))
+    app_logger.info(f"Remote file path: '{file_remote}'")
     if api.file.exists(TEAM_ID, file_remote):
-        raise FileExistsError("File {!r} already exists in Team Files. Make sure you want to replace it. "
-                              "Please, remove it manually and run the app again."
-                              .format(file_remote))
+        raise FileExistsError(f"File '{file_remote}' already exists in Team Files. Make sure you want to replace it. "
+                              "Please, remove it manually and run the app again.")
 
     file_local = os.path.join(my_app.data_dir, file_remote.lstrip("/"))
-    app_logger.info("Local file path: {!r}".format(file_local))
+    app_logger.info(f"Local file path: '{file_local}'")
     sly.fs.ensure_base_path(file_local)
     result_act.to_csv(file_local, index=False, header=True)
-    file_info = api.file.upload(TEAM_ID, file_local, file_remote)
+
+    upload_progress = []
+    def _print_progress(monitor, upload_progress):
+        if len(upload_progress) == 0:
+            upload_progress.append(sly.Progress(message=f"Upload '{RESULT_FILE_NAME}'",
+                                                total_cnt=monitor.len,
+                                                ext_logger=app_logger,
+                                                is_size=True))
+        upload_progress[0].set_current_value(monitor.bytes_read)
+
+    file_info = api.file.upload(TEAM_ID, file_local, file_remote, progress_cb=lambda m: _print_progress(m, upload_progress))
     api.task._set_custom_output(task_id, file_info.id, sly.fs.get_file_name_with_ext(file_remote),
                                 description="CSV with reference items")
 
